4e26e07bdf3774d7dc10c8208c0f2fa630b84c24
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2022-01-22 10:10:06 +0700

    add root to amr tokens

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,1 @@
-AMR_TOKENS = [':ARG0',':ARG1',':mod',':time', ':name', ':location', ':op1', ':op2', ':multi-sentence']
+AMR_TOKENS = [':ARG0',':ARG1',':mod',':time', ':name', ':location', ':op1', ':op2', ':root']
