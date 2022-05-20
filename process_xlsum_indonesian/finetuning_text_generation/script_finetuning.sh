f198a7931ada22b56a472d00fac3124501bd4d81
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-05-20 18:37:45 +0700

    Update script_finetuning.sh

--- process_xlsum_indonesian/finetuning_text_generation/script_finetuning.sh
+++ process_xlsum_indonesian/finetuning_text_generation/script_finetuning.sh
@@ -3,7 +3,7 @@ cd ../../tree_level_embeddings
 
 python train_indoT5.py \
 --model_type indo-t5 \
---n_epochs 2 --lr 0.0001 \
+--n_epochs 1 --lr 0.0001 \
 --data_folder ../process_xlsum_indonesian/finetuning_text_generation/data_train \
 --result_folder ../process_xlsum_indonesian/finetuning_text_generation/result \
 --resume_from_checkpoint True \
